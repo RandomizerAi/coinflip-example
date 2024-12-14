5622f40041cbb6c75fa83a83b0288d4d6b34af66
deanpress <hello@dean.press>
2024-12-14 22:46:45 +0400

    remove chunking from vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,27 +17,4 @@ export default defineConfig({
       usePolling: true,
     },
   },
-  build: {
-    // Enable chunk splitting
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          ethers: ["ethers"],
-          web3modal: ["web3modal"],
-          walletconnect: ["@walletconnect/web3-provider"],
-          vendor: ["socket.io-client"],
-        },
-      },
-    },
-    // Enable chunk size warnings
-    chunkSizeWarningLimit: 1000,
-    // Enable minification
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-  },
 });
